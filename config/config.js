2d14bd740db07f4400de3a0820781d62d5e95741
hrsoftengr <himanshurajput56@gmail.com>
2018-02-28 15:19:58 +0530

    himanshu

---
+++ config/config.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const  path = require ('path');
+
+const config = {
+    db:{
+        URL: 'mongodb://localhost/NgoHelp',
+        DEBUG: true,
+        autoIndex:true,
+        log:true
+    },
+    server:{
+        PORT : 3001,
+        HOSTNAME: 'http://localhost:3001/'
+    }
+};
+
+module.exports = config;
